9dc20c107c6c4169c40cfa473140f2adc7968be3
Kameron Clark <75962034+stumps290@users.noreply.github.com>
2022-02-15 10:28:11 -0800

    Update #1
    Got the actual framework of the code to work and it can now be scaled up

--- Clicker game/Clicker Game.py
+++ Clicker game/Clicker Game.py
@@ -3,62 +3,50 @@ import time
 
 #Guts of the code
 
-def price_increase(self):
-    """Increases the price of each auto clicker tier"""
-    self.price= self.price * self.price_increment
+Points=0
 
-class AutoClicker:
-    def __init__(self,name,price,price_increment,):
-        """
-        name=Name of the tier
-        price=The current price of the tier
-        price_increment=The exponent that the price gows at for each upgrade 
-        """
+#Classes
 
-        self.name = name
-        self.price = price
-        self.price_increment = price_increment
+class AutoClickers:
+    def __init__(self,price,counter,multiplier,value):
+        self.price=price
+        self.counter=counter
+        self.multiplier=multiplier
+        self.value=value
+#Objects
 
-Clicker1 = AutoClicker("tier 1",10,1.125)
+Upgrade_1 = AutoClickers(10,0,1.25,1)
 
-def increase():
-    """Increases point counter with each click"""
-    value = int(Count_Label["text"])
-    Count_Label["text"] = f"{value + 1}"
+#Functions
 
+def Click():
+    #called by click button, adds one to total point each button press
+    global Points
+    Points += 1
+    Points_Label.config(text = Points) #Updates points label
 
-def Tier1_click():
-    """Checks if you have enough points to buy a clicker"""
-    value = int(Count_Label["text"])
-    def Auto1():
-        global T1
-        T1=True
-        print(T1)
-        return T1
-    if value >= 10:
-        Count_Label["text"] = f"{value - Clicker1.price}"
-        Tier1_click.Tier1Amnt = int(Tier1_Label["text"])
-        Tier1_Label["text"] = f"{Tier1_click.Tier1Amnt + 1}"
-        Auto1()
+def Upgrade_1_Click(): #Called by tier 1 button
+    global Points
+    if Points >= Upgrade_1.price: #Checks if you have enough points to buy upgrade
+        Points -= Upgrade_1.price #Subtracts cost from total points
+        Upgrade_1.price = round(Upgrade_1.price * Upgrade_1.multiplier) #Updates new price by multiplying and then rounding down
+        Upgrade_1.counter += 1 #adds 1 to the tier counter
+        Tier1.config(text="Tier 1 Autobuyer\nCost:"+str(Upgrade_1.price)+"\n Amount:"+str(Upgrade_1.counter)) #Updates autoclicker label
+        Points_Label.config(text = Points) #Updates points value
 
 
 
-#Autoclickers
+#Loops
 
-global T1
-T1=False
-print(T1)
-timeout=1
+def Autoadder():
+    global Points
+    #adds the weight of the autoclicker multiplied by the amount bought
+    Points += Upgrade_1.value*Upgrade_1.counter
+    #Updates Points
+    Points_Label.config(text = Points)
+    #waits a second and then loops
+    window.after(1000,Autoadder)
 
-while T1==True:
-    #Tier 1
-    print("Running")
-    global Count
-    Count = int(Count_Label["text"])
-    print(Count)
-    Count_Label["text"] = (Count + Tier1_click.Tier1Amnt)
-    time.sleep(timeout)
-            
 
 #GUI
 
@@ -71,10 +59,9 @@ Title=tk.Label(master=Frame1,text="Clicker Game")
 Title.pack()
 
 #Counter
-
 Frame2=tk.Frame(master=window)
-Count_Label=tk.Label(master=Frame2,text="0")
-Count_Label.pack()
+Points_Label=tk.Label(master=Frame2,text=Points)
+Points_Label.pack()
 
 #Button 1
 Frame3=tk.Frame(master=window,)
@@ -85,7 +72,7 @@ MainClick=tk.Button(
     width=30,
     fg="black",
     bg="grey",
-    command=increase
+    command=Click
 )
 MainClick.pack()
 
@@ -93,25 +80,21 @@ MainClick.pack()
 Frame4=tk.Frame(master=window)
 Tier1=tk.Button(
     master=Frame4,
-    text="Tier 1 Autobuyer\nCost:"+str(Clicker1.price),
-    height=2,
+    text="Tier 1 Autobuyer\nCost:"+str(Upgrade_1.price)+"\n Amount:"+str(Upgrade_1.counter),
+    height=3,
     width=30,
     fg="black",
     bg="grey",
-    command=Tier1_click
+    command=Upgrade_1_Click
 )
 Tier1.pack()
-Frame5=tk.Frame(master=window)
-Tier1_Label=tk.Label(
-    master=Frame5,
-    text="0"
-)
-Tier1_Label.pack()
+
 
 Frame1.pack()
 Frame2.pack()
 Frame3.pack()
 Frame4.pack()
-Frame5.pack()
+
+Autoadder()
 
 window.mainloop()
